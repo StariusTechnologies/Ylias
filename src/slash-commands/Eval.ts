542e8bf6b4f3b381df8aea6cc9e7aa80ec899d77
Lily Wonhalf <lilywonhalf@gmail.com>
2021-08-10 05:22:37 -0400

    Added Eval command

---
+++ src/slash-commands/Eval.ts
@@ -0,0 +1,53 @@
+import { CommandInteraction, MessageEmbed } from 'discord.js';
+import { PieceContext } from '@sapphire/pieces';
+import { SlashCommand } from '../models/framework/lib/structures/SlashCommand';
+import Logger from '@lilywonhalf/pretty-logger';
+
+export default class EvalCommand extends SlashCommand {
+    constructor(context: PieceContext) {
+        super(context, {
+            description: 'Allows you to execute nodejs code and display the return value.',
+            defaultPermission: false,
+            arguments: [{
+                name: 'code',
+                description: 'The nodejs code you want to execute',
+                type: 'STRING',
+                required: true,
+            }],
+            permissions: [{
+                type: 'USER',
+                id: process.env.MOM,
+                permission: true,
+            }],
+        });
+    }
+
+    async run(interaction: CommandInteraction): Promise<void> {
+        let output;
+        const embed = new MessageEmbed()
+            .setTitle('✅ Code executed')
+            .setColor(0xE88745)
+            .setFields({
+                name: 'Executed code',
+                value: `\`\`\`js\n${interaction.options.getString('code')}\`\`\``,
+            });
+
+        try {
+            output = eval(interaction.options.getString('code'));
+        } catch (error) {
+            Logger.exception(error);
+            output = error.message;
+            embed.setTitle('❌ Code crashed');
+        }
+
+        if (!output || output.trim().length < 1) {
+            output = '<empty>';
+        }
+
+        embed.addField('Result', output);
+
+        await interaction.reply({
+            embeds: [embed],
+        });
+    }
+}
