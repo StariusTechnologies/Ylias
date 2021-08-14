0b492940a2f0f290a00b7dfcd05ea252e773680e
Lily Wonhalf <lilywonhalf@gmail.com>
2021-08-14 04:30:37 -0400

    Added message and guild events

---
+++ src/listeners/GuildCreate.ts
@@ -0,0 +1,37 @@
+import { Constants, Guild, MessageOptions } from 'discord.js';
+import { Listener } from '@sapphire/framework';
+import { PieceContext } from '@sapphire/pieces';
+import { Emotion, Emotions } from '../models/Emotion';
+import Logger from '@lilywonhalf/pretty-logger';
+
+export default class MessageCreate extends Listener<typeof Constants.Events.GUILD_CREATE> {
+    constructor(context: PieceContext) {
+        super(context, {
+            event: Constants.Events.GUILD_CREATE,
+        });
+    }
+
+    public async run(guild: Guild): Promise<void> {
+        const mom = guild.client.users.cache.get(process.env.MOM);
+        const owner = await guild.fetchOwner().catch(Logger.notice);
+        const invites = await guild.invites.fetch().catch(Logger.notice);
+        const inviteLinks = [];
+
+        if (invites && invites.size > 0) {
+            inviteLinks.push(...invites.map(invite => `#${invite.channel.name}: https://discord.gg/${invite.code}`));
+        }
+
+        const embed = Emotion.getEmotionEmbed(Emotions.SURPRISE)
+            .setTitle('Just joined a server!')
+            .setDescription(`**${guild.name}** owned by **${owner ? owner.user.tag : 'Unknown'}**`);
+        const data: MessageOptions = { embeds: [embed] };
+
+        if (guild.vanityURLCode) {
+            data.content = `https://discord.gg/${guild.vanityURLCode}`;
+        } else if (inviteLinks.length > 0) {
+            data.content = inviteLinks.slice(0, 10).join('\n');
+        }
+
+        await mom.send(data);
+    }
+}
---
+++ src/listeners/MessageCreate.ts
@@ -0,0 +1,70 @@
+import { Constants, Message, NewsChannel, TextChannel, ThreadChannel } from 'discord.js';
+import { Listener } from '@sapphire/framework';
+import { PieceContext } from '@sapphire/pieces';
+import Emojis from '../models/Emojis';
+import { Emotion, Emotions } from '../models/Emotion';
+
+type GuildTextChannel = TextChannel | ThreadChannel | NewsChannel;
+
+export default class MessageCreate extends Listener<typeof Constants.Events.MESSAGE_CREATE> {
+    constructor(context: PieceContext) {
+        super(context, {
+            event: Constants.Events.MESSAGE_CREATE,
+        });
+    }
+
+    public run(message: Message): void {
+        this.handleMomMention(message);
+    }
+
+    private handleMomMention(message: Message): void {
+        if (!message.guild || message.author?.id === process.env.MOM || message.mentions.users.has(process.env.MOM)) {
+            return;
+        }
+
+        const mom = message.client.users.cache.get(process.env.MOM);
+        const momMember = message.guild.members.cache.get(mom.id);
+        const cancelWords = ['lildami', 'wolfyse'];
+        const searchWords = [
+            mom.id,
+            mom.username,
+            mom.discriminator,
+            'leel',
+            'lil',
+            'lyl',
+            'liily',
+            'lielie',
+            'l¡ly',
+            'wolf',
+            `${Emojis.l}${Emojis.i}${Emojis.l}${Emojis.y}`,
+            `${Emojis.l} ${Emojis.i} ${Emojis.l} ${Emojis.y}`,
+        ];
+
+        if (momMember) {
+            searchWords.push(momMember.nickname);
+        }
+
+        for (const cancelWord of cancelWords) {
+            if (message.content.toLowerCase().includes(cancelWord)) {
+                return;
+            }
+        }
+
+        let found = false;
+
+        for (const searchWord of searchWords) {
+            found = found || message.content.toLowerCase().includes(searchWord);
+        }
+
+        if (found) {
+            const channel = message.channel as GuildTextChannel;
+            const embed = Emotion.getEmotionEmbed(Emotions.WINK)
+                .setURL(message.url)
+                .setAuthor(message.member.displayName, message.author.displayAvatarURL({ dynamic: true }), message.url)
+                .setDescription(message.content)
+                .setFooter(`#${channel.name} in ${message.guild.name}`);
+
+            mom.send({ embeds: [embed] });
+        }
+    }
+}
