72ea93dd260024e7e3e1425aa39b5fb527cfa772
Lily Wonhalf <lilywonhalf@gmail.com>
2021-08-14 03:24:47 -0400

    Added Youtube command

---
+++ src/slash-commands/YouTube.ts
@@ -0,0 +1,64 @@
+import { CommandInteraction } from 'discord.js';
+import { PieceContext } from '@sapphire/pieces';
+import { fetch } from '@sapphire/fetch';
+import { BucketScope } from '@sapphire/framework';
+import { SlashCommand } from '../models/framework/lib/structures/SlashCommand';
+import { Emotion, Emotions } from '../models/Emotion';
+import Logger from "@lilywonhalf/pretty-logger";
+
+export default class YouTubeCommand extends SlashCommand {
+    private static readonly YOUTUBE_VIDEO_KIND = 'youtube#video';
+
+    constructor(context: PieceContext) {
+        super(context, {
+            description: 'Allows you to search a video on YouTube.',
+            arguments: [{
+                name: 'query',
+                description: 'The search query',
+                type: 'STRING',
+                required: true,
+            }],
+            preconditions: [{
+                name: 'Cooldown',
+                context: {
+                    scope: BucketScope.User,
+                    delay: 5000,
+                },
+            }],
+        });
+    }
+
+    public async run(interaction: CommandInteraction): Promise<void> {
+        const videoURL = 'https://www.youtube.com/watch?v=';
+        const apiBaseURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50`;
+        const query = encodeURIComponent(interaction.options.getString('query'));
+        const apiURL = `${apiBaseURL}&key=${process.env.GOOGLE_API_KEY}&q=${query}`;
+        const response: any = await fetch(apiURL);
+        const errorEmbed = Emotion.getEmotionEmbed(Emotions.SAD).setTitle('YouTube');
+
+        if (response?.items) {
+            let link;
+
+            for (const item of response.items) {
+                if (item.id.kind === YouTubeCommand.YOUTUBE_VIDEO_KIND) {
+                    link = videoURL + item.id.videoId;
+                }
+            }
+
+            if (link) {
+                await interaction.reply(link);
+            } else {
+                await interaction.reply({ embeds: [errorEmbed.setDescription('No results found.')] });
+            }
+        } else {
+            Logger.exception(response);
+
+            const errorMessage = `YouTube sent an error: ${response.error}`;
+            const failedMessage = `It seems like YouTube is not working.`;
+
+            errorEmbed.setDescription(response.error ? errorMessage : failedMessage);
+
+            await interaction.reply({ embeds: [errorEmbed] });
+        }
+    }
+}
